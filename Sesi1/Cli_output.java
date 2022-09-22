ed6160a8d77360423b74782f39d102f09d153817
iqbalramdhan <iqbalramdhan0412@gmail.com>
2022-09-22 19:01:29 +0700

    Add File Cli_output My Task

---
+++ Sesi1/Cli_output.java
@@ -0,0 +1,9 @@
+package Sesi1;
+
+public class Cli_output{
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    }
+}
---
+++ Sesi1/Gui_output.java
@@ -0,0 +1,9 @@
+package Sesi1;
+import javax.swing.*;
+
+class Gui_output{
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("Anda sedang belajar apa? ");
+        JOptionPane.showMessageDialog(null,"Belajar " + input + " sangat mudah");
+    }
+}
