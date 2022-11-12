d31e2c0cf4a7e27aa53a13ef8c7648c40c754160
iqbalramdhan <iqbalramdhan0412@gmail.com>
2022-11-12 08:54:43 +0700

    add file Person.java

---
+++ Sesi5/Kampus/Person.java
@@ -0,0 +1,28 @@
+package Kampus;
+
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setName(String address) {
+        this.address = address;
+    }
+
+    public String toString() {
+        return "Nama: " + getName() +
+                "\nAlamat: " + getAddress();
+    }
+}
