109424373e1fdde8db7f634b2cd83009266d413c
iqbalramdhan <iqbalramdhan0412@gmail.com>
2022-11-12 08:54:12 +0700

    add file Main.java

---
+++ Sesi5/Kampus/Main.java
@@ -0,0 +1,32 @@
+/*
+Nama    = Muhamad Iqbal Ramadhan
+NIM     = 20210040139
+Kelas   = TI21G
+ */
+package Kampus;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("masukan nama dosen : ");
+        String nama = sc.nextLine();
+        System.out.print("masukan alamat dosen : ");
+
+        String alamat = sc.nextLine();
+
+        Dosen dosen1 = new Dosen(nama, alamat);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("masukan mata kuliah yang diampu: ");
+            String matkul = sc.nextLine();
+            dosen1.addCourse(matkul);
+        }
+
+        System.out.println(dosen1);
+        sc.close();
+    }
+}
